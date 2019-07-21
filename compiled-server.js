0443809f7942e5eaa4b68f31b74a03e53518dbd2
Avshar K <avshark@gmail.com>
2019-07-20 20:06:10 -0400

    Server Sign In
    Co-authored-by: Jaron Truman <joltruman88@gmail.com>
    Co-authored-by: Daniel Rote <drote.email@gmail.com>

--- compiled-server.js
+++ compiled-server.js
@@ -8,8 +8,13 @@ var _nodeFetch = _interopRequireDefault(require("node-fetch"));
 
 var _cors = _interopRequireDefault(require("cors"));
 
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+var _bcrypt = _interopRequireDefault(require("bcrypt"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+var saltRounds = 2;
 var server = (0, _express["default"])();
 server.use((0, _cors["default"])());
 server.use(_express["default"].json());
@@ -17,22 +22,102 @@ var PORT = 3001;
 var config = {
   couchBaseUrl: 'http://localhost:5984/',
   admin: 'admin',
-  adminPass: 'admin'
+  adminPass: 'admin',
+  tokenKey: '12345678'
 };
+server.use(function (req, res, next) {
+  try {
+    var token = req.headers.authorization.split(' ')[1];
+
+    _jsonwebtoken["default"].verify(token, config.tokenKey, function (err, payload) {
+      console.log(payload);
+
+      if (payload) {
+        var options = {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: 'Basic ' + (0, _btoa["default"])('admin:admin')
+          }
+        };
+        return (0, _nodeFetch["default"])("http://127.0.0.1:5984/pouchdb_users/".concat(payload.userId), options).then(function (response) {
+          console.log('[RESPONSE] ', response);
+          req.user = response.body;
+          console.log(req.user);
+          next();
+        })["catch"](function (error) {
+          console.log('[ERROR] ', error);
+        });
+      } else {
+        next();
+      }
+    });
+  } catch (_unused) {
+    next();
+  }
+});
+server.post('/signin', function (req, res) {
+  var options = {
+    method: 'POST',
+    body: JSON.stringify({
+      "selector": {
+        "username": req.body.username
+      },
+      "fields": ["password", "_id"],
+      "limit": 1
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: 'Basic ' + (0, _btoa["default"])('admin:admin')
+    }
+  };
+  return (0, _nodeFetch["default"])("http://127.0.0.1:5984/pouchdb_users/_find", options).then(function (response) {
+    console.log('[RESPONSE] ', response.json().then(function (res) {
+      console.log(res);
+    }));
+    var user = response.body.docs[0];
+
+    if (_bcrypt["default"].compareSync(req.body.password, user.password)) {
+      var token = _jsonwebtoken["default"].sign({
+        userId: user._id
+      }, config.tokenKey);
+
+      res.status(200).json({
+        userId: user._id,
+        username: user.username,
+        token: token
+      });
+    } else {
+      res.status(400).json({
+        message: 'Invalid Password/Username'
+      });
+    }
+  })["catch"](function (error) {
+    console.log('[ERROR] ', error);
+    res.status(400).json({
+      message: 'Invalid Password/Username'
+    });
+  });
+});
 server.post('/signup', function (req, res) {
   console.log('[REQUEST BODY] ', req.body);
   console.log('------------------');
   var username = req.body.username;
-  var password = req.body.password;
-  var url = "http://127.0.0.1:5984/_users/org.couchdb.user:".concat(username);
+  var password = req.body.password; // let salt = bcrypt.genSaltSync(saltRounds);
+  // let url = `http://127.0.0.1:5984/_users/org.couchdb.user:${username}`;
+
+  var url = "http://127.0.0.1:5984/pouchdb_users/";
   var data = {
-    name: username,
-    password: password,
-    roles: [],
-    type: 'user'
-  };
+    username: username,
+    password: _bcrypt["default"].hashSync(password, saltRounds) // roles: [],
+    // type: 'user'
+
+  }; // bcrypt.compareSync(password, hash); true / false
+
   var options = {
-    method: 'PUT',
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json",
@@ -46,7 +131,8 @@ server.post('/signup', function (req, res) {
   })["catch"](function (error) {
     console.log('[ERROR] ', error);
   });
-});
+}); // server.post('/signin',)
+
 server.listen(PORT, function () {
   console.log("Cushion server is running on ".concat(PORT));
 });
--- server.js
+++ server.js
@@ -3,33 +3,117 @@ import btoa from 'btoa';
 import fetch from 'node-fetch';
 import cors from 'cors';
 
+import jwt from 'jsonwebtoken';
+
+import bcrypt from 'bcrypt';
+
+const saltRounds = 2;
+
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 const PORT = 3001;
 const config = {
-	couchBaseUrl: 'http://localhost:5984/',
-	admin: 'admin',
-	adminPass: 'admin'
+  couchBaseUrl: 'http://localhost:5984/',
+  admin: 'admin',
+  adminPass: 'admin',
+  tokenKey: '12345678'
 };
 
-server.post('/signup', (req, res) => {
-	console.log('[REQUEST BODY] ', req.body);
-	console.log('------------------');
-	const username = req.body.username;
-	const password = req.body.password;
+server.use((req,res,next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, config.tokenKey, (err, payload) => {
+      console.log(payload);
 
-  let url = `http://127.0.0.1:5984/_users/org.couchdb.user:${username}`;
-	let data = {
-		name: username,
-		password: password,
-		roles: [],
-		type: 'user'
-	};
+      if(payload){
+        let options = {
+          method: 'GET',
+          headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: 'Basic '+btoa('admin:admin')
+          }
+        }
+        return fetch(`http://127.0.0.1:5984/pouchdb_users/${payload.userId}`, options)
+          .then(response =>  {
+            console.log('[RESPONSE] ', response);
+            req.user = response.body;
+            console.log(req.user);
+            next();
+          }).catch(error => {
+            console.log('[ERROR] ', error);
+          });
+      } else {
+        next();
+      }
+    });
+  } catch {
+    next();
+  }
+});
+
+server.post('/signin',function(req,res){
+  let options = {
+    method: 'POST',
+    body: JSON.stringify({
+      "selector":{
+        "username": req.body.username ,
+      },
+      "fields": ["password", "_id"],
+      "limit":1,
+    }),
+    headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: 'Basic '+btoa('admin:admin')
+    }
+  }
+  return fetch(`http://127.0.0.1:5984/pouchdb_users/_find`, options)
+    .then(response =>  {
+      console.log('[RESPONSE] ', response.json().then(res =>{
+        console.log(res);
+      } ));
+      const user = response.body.docs[0];
+      if(bcrypt.compareSync(req.body.password, user.password)){
+        let token = jwt.sign({userId:user._id}, config.tokenKey);
+        res.status(200).json({
+          userId: user._id,
+          username: user.username,
+          token
+        })
+      } else {
+        res.status(400).json({message:'Invalid Password/Username'});
+      }
+    }).catch(error => {
+      console.log('[ERROR] ', error);
+      res.status(400).json({message:'Invalid Password/Username'});
+    });
+});
+
+server.post('/signup', (req, res) => {
+  console.log('[REQUEST BODY] ', req.body);
+  console.log('------------------');
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  // let salt = bcrypt.genSaltSync(saltRounds);
+
+  // let url = `http://127.0.0.1:5984/_users/org.couchdb.user:${username}`;
+  let url = `http://127.0.0.1:5984/pouchdb_users/`;
+  let data = {
+    username: username,
+    password: bcrypt.hashSync(password, saltRounds),
+    // roles: [],
+    // type: 'user'
+  };
+
+  // bcrypt.compareSync(password, hash); true / false
 
   let options = {
-    method: 'PUT',
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json",
@@ -39,13 +123,14 @@ server.post('/signup', (req, res) => {
   };
 
   return fetch(url, options).then(response =>  {
-		console.log('[RESPONSE] ', response);
-		res.send(response);
-	}).catch(error => {
-		console.log('[ERROR] ', error);
-	});
+    console.log('[RESPONSE] ', response);
+    res.send(response);
+  }).catch(error => {
+    console.log('[ERROR] ', error);
+  });
 })
 
+// server.post('/signin',)
 server.listen(PORT, () => {
-	console.log(`Cushion server is running on ${PORT}`);
+  console.log(`Cushion server is running on ${PORT}`);
 });
