aa73973cacd70ad04f0be256d8e8f9d2f868a80a
drote <drote.email@gmail.com>
2019-11-17 20:02:39 -0800

    https

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const server = require('./server');
 const https = require('https');
 const fs = require('fs');
+const utils = require('./utils/utils');
+
+const envVars = utils.getEnvVars();
+const key = fs.readFileSync('../key.pem');
+const cert = fs.readFileSync('../cert.pem');
+const passphrase = envVars.httpsPass;
 
 const PORT = 3001;
 
-server.listen(PORT, () => {
+https.createServer({key, cert, passphrase}, server).listen(PORT, () => {
   console.log(`-- Cushion server is running on port ${PORT}`);
 
   if (process.env.NODE_ENV === 'production') {
